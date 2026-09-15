e70fd8ed660dc4a0849afb53d733a645d10abc87
Alfar0nt <mamank1243@gmail.com>
2026-09-15 16:57:48 +0700

    update module 4 sesuai tugas

--- ExerciseModule4/ExerciseModule4/Form1.Designer.vb
+++ ExerciseModule4/ExerciseModule4/Form1.Designer.vb
@@ -44,90 +44,102 @@ Partial Class Form1
         ColumnHeader4 = New ColumnHeader()
         ColumnHeader5 = New ColumnHeader()
         ColumnHeader6 = New ColumnHeader()
+        ColumnHeader7 = New ColumnHeader()
         btPrint = New Button()
+        Label7 = New Label()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(27, 32)
+        Label1.Location = New Point(46, 52)
+        Label1.Margin = New Padding(5, 0, 5, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(59, 25)
+        Label1.Size = New Size(96, 41)
         Label1.TabIndex = 0
         Label1.Text = "Nama"
         ' 
         ' tbName
         ' 
-        tbName.Location = New Point(121, 26)
+        tbName.Location = New Point(206, 43)
+        tbName.Margin = New Padding(5)
         tbName.Name = "tbName"
-        tbName.Size = New Size(150, 31)
+        tbName.Size = New Size(252, 47)
         tbName.TabIndex = 1
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(27, 110)
+        Label2.Location = New Point(46, 180)
+        Label2.Margin = New Padding(5, 0, 5, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(51, 25)
+        Label2.Size = New Size(84, 41)
         Label2.TabIndex = 2
         Label2.Text = "NPM"
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(24, 174)
+        Label3.Location = New Point(41, 285)
+        Label3.Margin = New Padding(5, 0, 5, 0)
         Label3.Name = "Label3"
-        Label3.Size = New Size(54, 25)
+        Label3.Size = New Size(88, 41)
         Label3.TabIndex = 3
         Label3.Text = "Prodi"
         ' 
         ' tbNPM
         ' 
-        tbNPM.Location = New Point(121, 112)
+        tbNPM.Location = New Point(206, 184)
+        tbNPM.Margin = New Padding(5)
         tbNPM.Name = "tbNPM"
-        tbNPM.Size = New Size(150, 31)
+        tbNPM.Size = New Size(252, 47)
         tbNPM.TabIndex = 4
         ' 
         ' tbProdi
         ' 
-        tbProdi.Location = New Point(121, 174)
+        tbProdi.Location = New Point(206, 285)
+        tbProdi.Margin = New Padding(5)
         tbProdi.Name = "tbProdi"
-        tbProdi.Size = New Size(150, 31)
+        tbProdi.Size = New Size(252, 47)
         tbProdi.TabIndex = 5
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(306, 32)
+        Label4.Location = New Point(520, 52)
+        Label4.Margin = New Padding(5, 0, 5, 0)
         Label4.Name = "Label4"
-        Label4.Size = New Size(116, 25)
+        Label4.Size = New Size(195, 41)
         Label4.TabIndex = 6
         Label4.Text = "Jenis Kelamin"
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Location = New Point(306, 110)
+        Label5.Location = New Point(520, 180)
+        Label5.Margin = New Padding(5, 0, 5, 0)
         Label5.Name = "Label5"
-        Label5.Size = New Size(85, 25)
+        Label5.Size = New Size(141, 41)
         Label5.TabIndex = 7
         Label5.Text = "Semester"
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
-        Label6.Location = New Point(306, 174)
+        Label6.Location = New Point(520, 285)
+        Label6.Margin = New Padding(5, 0, 5, 0)
         Label6.Name = "Label6"
-        Label6.Size = New Size(102, 25)
+        Label6.Size = New Size(171, 41)
         Label6.TabIndex = 8
         Label6.Text = "No Telepon"
         ' 
         ' RbLakilaki
         ' 
         RbLakilaki.AutoSize = True
-        RbLakilaki.Location = New Point(460, 26)
+        RbLakilaki.Location = New Point(782, 43)
+        RbLakilaki.Margin = New Padding(5)
         RbLakilaki.Name = "RbLakilaki"
-        RbLakilaki.Size = New Size(114, 29)
+        RbLakilaki.Size = New Size(185, 45)
         RbLakilaki.TabIndex = 9
         RbLakilaki.TabStop = True
         RbLakilaki.Text = "Laki - Laki"
@@ -136,9 +148,10 @@ Partial Class Form1
         ' RbPerempuan
         ' 
         RbPerempuan.AutoSize = True
-        RbPerempuan.Location = New Point(460, 61)
+        RbPerempuan.Location = New Point(782, 100)
+        RbPerempuan.Margin = New Padding(5)
         RbPerempuan.Name = "RbPerempuan"
-        RbPerempuan.Size = New Size(126, 29)
+        RbPerempuan.Size = New Size(206, 45)
         RbPerempuan.TabIndex = 10
         RbPerempuan.TabStop = True
         RbPerempuan.Text = "Perempuan"
@@ -148,42 +161,47 @@ Partial Class Form1
         ' 
         CbSemester.DropDownStyle = ComboBoxStyle.DropDownList
         CbSemester.FormattingEnabled = True
-        CbSemester.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8"})
-        CbSemester.Location = New Point(460, 110)
+        CbSemester.Location = New Point(782, 180)
+        CbSemester.Margin = New Padding(5)
         CbSemester.Name = "CbSemester"
-        CbSemester.Size = New Size(182, 33)
+        CbSemester.Size = New Size(307, 49)
         CbSemester.TabIndex = 11
         ' 
         ' tbTelepon
         ' 
-        tbTelepon.Location = New Point(460, 174)
+        tbTelepon.Location = New Point(782, 285)
+        tbTelepon.Margin = New Padding(5)
         tbTelepon.Name = "tbTelepon"
-        tbTelepon.Size = New Size(182, 31)
+        tbTelepon.Size = New Size(307, 47)
         tbTelepon.TabIndex = 12
         ' 
         ' CheckedListBox1
         ' 
         CheckedListBox1.FormattingEnabled = True
-        CheckedListBox1.Location = New Point(682, 26)
+        CheckedListBox1.Items.AddRange(New Object() {"e-sport", "renang", "basket", "volly", "UKKI"})
+        CheckedListBox1.Location = New Point(1159, 43)
+        CheckedListBox1.Margin = New Padding(5)
         CheckedListBox1.Name = "CheckedListBox1"
-        CheckedListBox1.Size = New Size(195, 200)
+        CheckedListBox1.Size = New Size(329, 312)
         CheckedListBox1.TabIndex = 13
         ' 
         ' btnOk
         ' 
-        btnOk.Location = New Point(31, 248)
+        btnOk.Location = New Point(53, 407)
+        btnOk.Margin = New Padding(5)
         btnOk.Name = "btnOk"
-        btnOk.Size = New Size(846, 34)
+        btnOk.Size = New Size(1438, 56)
         btnOk.TabIndex = 14
         btnOk.Text = "Save"
         btnOk.UseVisualStyleBackColor = True
         ' 
         ' lvDataMahasiswa
         ' 
-        lvDataMahasiswa.Columns.AddRange(New ColumnHeader() {ColumnHeader1, ColumnHeader2, ColumnHeader3, ColumnHeader4, ColumnHeader5, ColumnHeader6})
-        lvDataMahasiswa.Location = New Point(36, 306)
+        lvDataMahasiswa.Columns.AddRange(New ColumnHeader() {ColumnHeader1, ColumnHeader2, ColumnHeader3, ColumnHeader4, ColumnHeader5, ColumnHeader6, ColumnHeader7})
+        lvDataMahasiswa.Location = New Point(61, 502)
+        lvDataMahasiswa.Margin = New Padding(5)
         lvDataMahasiswa.Name = "lvDataMahasiswa"
-        lvDataMahasiswa.Size = New Size(841, 146)
+        lvDataMahasiswa.Size = New Size(1427, 237)
         lvDataMahasiswa.TabIndex = 15
         lvDataMahasiswa.UseCompatibleStateImageBehavior = False
         lvDataMahasiswa.View = View.Details
@@ -196,17 +214,17 @@ Partial Class Form1
         ' ColumnHeader2
         ' 
         ColumnHeader2.Text = "Nama"
-        ColumnHeader2.Width = 110
+        ColumnHeader2.Width = 250
         ' 
         ' ColumnHeader3
         ' 
         ColumnHeader3.Text = "Prodi"
-        ColumnHeader3.Width = 120
+        ColumnHeader3.Width = 250
         ' 
         ' ColumnHeader4
         ' 
         ColumnHeader4.Text = "Jenis Kelamin"
-        ColumnHeader4.Width = 130
+        ColumnHeader4.Width = 200
         ' 
         ' ColumnHeader5
         ' 
@@ -216,22 +234,39 @@ Partial Class Form1
         ' ColumnHeader6
         ' 
         ColumnHeader6.Text = "No Telepon"
-        ColumnHeader6.Width = 160
+        ColumnHeader6.Width = 250
+        ' 
+        ' ColumnHeader7
+        ' 
+        ColumnHeader7.Text = "UKM"
+        ColumnHeader7.Width = 230
         ' 
         ' btPrint
         ' 
-        btPrint.Location = New Point(765, 482)
+        btPrint.Location = New Point(1300, 790)
+        btPrint.Margin = New Padding(5)
         btPrint.Name = "btPrint"
-        btPrint.Size = New Size(112, 34)
+        btPrint.Size = New Size(190, 56)
         btPrint.TabIndex = 16
         btPrint.Text = "Tampilkan"
         btPrint.UseVisualStyleBackColor = True
         ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(1159, 8)
+        Label7.Margin = New Padding(5, 0, 5, 0)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(83, 41)
+        Label7.TabIndex = 17
+        Label7.Text = "UKM"
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(17F, 41F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(895, 540)
+        ClientSize = New Size(1522, 886)
+        Controls.Add(Label7)
         Controls.Add(btPrint)
         Controls.Add(lvDataMahasiswa)
         Controls.Add(btnOk)
@@ -249,6 +284,7 @@ Partial Class Form1
         Controls.Add(Label2)
         Controls.Add(tbName)
         Controls.Add(Label1)
+        Margin = New Padding(5)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
@@ -278,6 +314,8 @@ Partial Class Form1
     Friend WithEvents ColumnHeader4 As ColumnHeader
     Friend WithEvents ColumnHeader5 As ColumnHeader
     Friend WithEvents ColumnHeader6 As ColumnHeader
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ColumnHeader7 As ColumnHeader
 
     Private Sub tbName_TextChanged(sender As Object, e As EventArgs) Handles tbName.TextChanged
 
--- ExerciseModule4/ExerciseModule4/Form1.vb
+++ ExerciseModule4/ExerciseModule4/Form1.vb
@@ -2,7 +2,13 @@
     Dim arr(,) As String
     Dim i As Integer
     Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ReDim arr(20, 5)
+        ReDim arr(20, 6)
+
+        Dim semester() As Integer = {1, 2, 3, 4, 5, 6, 7, 8}
+        For t As Integer = 0 To semester.Length - 1
+            CbSemester.Items.Add(semester(t))
+        Next
+
     End Sub
 
     Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
@@ -18,18 +24,31 @@
 
         arr(i, 4) = CbSemester.Text
         arr(i, 5) = tbTelepon.Text
+
+        Dim strUkm As String = ""
+        For t As Integer = 0 To CheckedListBox1.Items.Count - 1
+            If CheckedListBox1.GetItemChecked(t) Then
+                If strUkm <> "" Then strUkm &= ", "
+                strUkm &= CheckedListBox1.Items(t).ToString()
+            End If
+        Next
+        arr(i, 6) = strUkm
+
         i += 1
         MsgBox("data sukses ditambah")
 
         lvDataMahasiswa.Items.Clear()
 
         For K As Integer = 0 To arr.GetUpperBound(0)
-            lvDataMahasiswa.Items.Add(arr(K, 0))
-            lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 1))
-            lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 2))
-            lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 3))
-            lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 4))
-            lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 5))
+            If arr(K, 0) <> Nothing Then
+                lvDataMahasiswa.Items.Add(arr(K, 0))
+                lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 1))
+                lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 2))
+                lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 3))
+                lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 4))
+                lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 5))
+                lvDataMahasiswa.Items(lvDataMahasiswa.Items.Count - 1).SubItems.Add(arr(K, 6))
+            End If
         Next
 
         Dim ctr As Control
@@ -40,6 +59,12 @@
                 DirectCast(ctr, ComboBox).SelectedItem = Nothing
             ElseIf TypeOf ctr Is RadioButton Then
                 DirectCast(ctr, RadioButton).Checked = False
+            ElseIf TypeOf ctr Is CheckedListBox Then
+                DirectCast(ctr, CheckedListBox).Items.Clear()
+                Dim ukm() As String = {"E-Sport", "Renang", "Basket", "Volly", "UKKI"}
+                For t As Integer = 0 To ukm.Length - 1
+                    DirectCast(ctr, CheckedListBox).Items.Add(ukm(t))
+                Next
             End If
         Next
     End Sub
@@ -47,13 +72,19 @@
     Private Sub btPrint_Click(sender As Object, e As EventArgs) Handles btPrint.Click
         Dim Str As String = ""
         For k As Integer = 0 To arr.GetUpperBound(0)
-            Str &= arr(k, 0) & " " & arr(k, 1) & " " & arr(k, 2) & arr(k, 3) & " " & arr(k, 4) & " " & arr(k, 5) & vbCrLf
+            If arr(k, 0) <> Nothing Then
+                Str &= arr(k, 0) & " " & arr(k, 1) & " " & arr(k, 2) & arr(k, 3) & " " & arr(k, 4) & " " & arr(k, 5) & " " & arr(k, 6) & vbCrLf
+            End If
         Next
 
         MsgBox(Str)
     End Sub
 
     Private Sub lvDataMahasiswa_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lvDataMahasiswa.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CheckedListBox1.SelectedIndexChanged
 
     End Sub
 End Class
